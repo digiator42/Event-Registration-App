c85cae35acd880097a6ae5246826ae241c23a27a
Ahmed Hassan <digiator2020@gmail.com>
2025-03-11 01:14:40 +0200

    feat: login & logout

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home_page, name='home'),
+    path('login', views.login_view, name='login'),
+    path('login', views.register_view, name='register'),
+    path('logout', views.logout_view, name='logout'),
     path('event/<str:pk>', views.event, name='event'),
     path('event-confirmation/<str:pk>', views.event_confirmation, name='event-confirmation'),
     path('profile/<str:pk>', views.profile, name='profile'),
--- base/views.py
+++ base/views.py
@@ -1,26 +1,47 @@
 from django.shortcuts import render, redirect
 from .models import User, Submission, Event
-from django.http import JsonResponse
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
+from typing import Dict
 
 # Create your views here.
-def spa_content(request, template, context):
+def _spa_content(request, template: str, context: Dict):
     if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        print('------> ', template)
         return render(request, template, context)
-    
+
     context['block_content'] = template
     return render(request, 'base.html', context)
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return _spa_content(request, 'login_register.html', {'form': form})
+
+def register_view(request):
+    pass
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
 
 def home_page(request):
     users = Submission.objects.all()
     events = Event.objects.all()
     context = {'users': users, 'events': events}
 
-    return spa_content(request, 'home.html', context)
+    return _spa_content(request, 'home.html', context)
 
 def event(request, pk):
     event = Event.objects.get(id=pk)
     context = {'event': event}
-    return spa_content(request, 'event.html', context)
+    return _spa_content(request, 'event.html', context)
 
 def event_confirmation(request, pk):
     event = Event.objects.get(id=pk)
@@ -29,9 +50,9 @@ def event_confirmation(request, pk):
         return redirect('event', pk=event.id)
     
     context = {'event': event}
-    return spa_content(request, 'event_confirmation.html', context)
+    return _spa_content(request, 'event_confirmation.html', context)
 
 def profile(request, pk):
     user_events = Event.objects.filter(participants=request.user)
     context = {'events': user_events}
-    return spa_content(request, 'profile.html', context)
+    return _spa_content(request, 'profile.html', context)
