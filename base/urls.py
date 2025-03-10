501a3773b508fb563aeb18c1ec895c0e72bda20e
Ahmed Hassan <digiator2020@gmail.com>
2025-03-10 03:42:28 +0200

    feat: event and profile views

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home_page, name='home'),
+    path('event/<str:pk>', views.event, name='event'),
+    path('event-confirmation/<str:pk>', views.event_confirmation, name='event-confirmation'),
+    path('profile/<str:pk>', views.profile, name='profile'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
-from .models import User, Submission
+from django.shortcuts import render, redirect
+from .models import User, Submission, Event
 
 # Create your views here.
 def home_page(request):
     users = Submission.objects.all()
-    context = {'users': users}
+    events = Event.objects.all()
+    context = {'users': users, 'events': events}
     return render(request, 'home.html', context)
+
+def event(request, pk):
+    event = Event.objects.get(id=pk)
+    context = {'event': event}
+    return render(request, 'event.html', context)
+
+def event_confirmation(request, pk):
+    event = Event.objects.get(id=pk)
+    if request.method == 'POST':
+        event.participants.add(request.user)
+        return redirect('event', pk=event.id)
+    context = {'event': event}
+    return render(request, 'event_confirmation.html', context)
+
+def profile(request, pk):
+    user_events = Event.objects.filter(participants=request.user)
+    context = {'events': user_events}
+    return render(request, 'profile.html', context)
