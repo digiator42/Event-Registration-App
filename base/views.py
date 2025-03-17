202bf785ec70a19d2a5c4e5a992742cc6e914bc2
Ahmed Hassan <digiator2020@gmail.com>
2025-03-17 21:41:59 +0200

    fetch

--- base/views.py
+++ base/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def _spa_content(request, template: str, context: Dict):
-    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+    if request.headers.get('X-Requested-With') == 'Fetch':
         return render(request, template, context)
 
     context['block_content'] = template
